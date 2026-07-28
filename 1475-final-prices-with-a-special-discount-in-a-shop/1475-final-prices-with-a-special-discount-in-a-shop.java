47908d1a8d9138cbf07b8ba6b204eb53dc853fc5
praneethp002-git <praneethp002@gmail.com>
2026-07-28 20:35:37 +0530

    Time: 1 ms (99.75%), Space: 45.1 MB (76.16%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] finalPrices(int[] p) {
+        for(int i=0;i<p.length;i++){
+            for(int j=i+1;j<p.length;j++){
+                if(p[i]>=p[j]){
+                    p[i]=p[i]-p[j];
+                    break;
+                }
+            }
+        }
+        return p;
+    }
+}
