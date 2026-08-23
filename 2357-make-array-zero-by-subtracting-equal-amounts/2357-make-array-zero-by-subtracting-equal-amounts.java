fb792df0c7c21cef6c0c7f24db7f57e1bccd71a3
praneethp002-git <praneethp002@gmail.com>
2026-08-23 19:31:12 +0530

    Time: 1 ms (92.54%), Space: 43.1 MB (55.37%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                hs.add(nums[i]);
+            }
+        }
+        return hs.size();
+    }
+}
